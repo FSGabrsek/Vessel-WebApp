9699b6e4ca52dc92aed77f9d4c30a4bf5a3433e3
FSGabrsek <f.gabrsek@gmail.com>
2022-11-10 14:44:27 +0100

    generated modules

---
+++ src/app/features/personal-list/personal-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PersonalListRoutingModule } from './personal-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PersonalListRoutingModule
+  ]
+})
+export class PersonalListModule { }
