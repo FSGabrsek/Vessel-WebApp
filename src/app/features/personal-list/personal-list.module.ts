14ea01fefccf35e63460b937e1c170dd8a9558e8
FSGabrsek <f.gabrsek@gmail.com>
2022-11-10 14:50:16 +0100

    added dependencies

--- src/app/features/personal-list/personal-list.module.ts
+++ src/app/features/personal-list/personal-list.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PersonalListRoutingModule } from './personal-list-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PersonalListRoutingModule
+    PersonalListRoutingModule,
+    SharedModule,
   ]
 })
 export class PersonalListModule { }
