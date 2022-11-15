5941beeea559c49783dd54a1e0b2324177c73b40
FSGabrsek <f.gabrsek@gmail.com>
2022-11-15 16:11:33 +0100

    added navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SharedModule,
-  ]
+    declarations: [
+        NavbarComponent
+    ],
+    imports: [
+        CommonModule,
+        SharedModule,
+    ],
+    exports: [
+        NavbarComponent
+    ]
 })
 export class CoreModule { }
