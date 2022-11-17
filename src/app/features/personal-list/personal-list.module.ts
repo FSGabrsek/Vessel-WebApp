0a65b68acab94faea07fbec20e4f54ada2abf5eb
FSGabrsek <f.gabrsek@gmail.com>
2022-11-17 16:39:25 +0100

    created view-media-page

--- src/app/features/personal-list/personal-list.module.ts
+++ src/app/features/personal-list/personal-list.module.ts
@@ -5,11 +5,13 @@ import { PersonalListRoutingModule } from './personal-list-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CreateMediaComponent } from './pages/create-media/create-media.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ViewMediaComponent } from './pages/view-media/view-media.component';
 
 
 @NgModule({
   declarations: [
-    CreateMediaComponent
+    CreateMediaComponent,
+    ViewMediaComponent
   ],
   imports: [
     CommonModule,
