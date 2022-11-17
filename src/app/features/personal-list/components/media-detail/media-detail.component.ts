0eaaa33c27f55b19163b7d97ebbed9d0db22849c
FSGabrsek <f.gabrsek@gmail.com>
2022-11-17 20:44:00 +0100

    added get feature to detail view

--- src/app/features/personal-list/components/media-detail/media-detail.component.ts
+++ src/app/features/personal-list/components/media-detail/media-detail.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MediaVessel } from '../../models/MediaVessel.model';
+import { MediaService } from '../../services/media.service';
 
 @Component({
-  selector: 'app-media-detail',
-  templateUrl: './media-detail.component.html',
-  styleUrls: ['./media-detail.component.scss']
-})
+    selector: 'app-media-detail',
+    templateUrl: './media-detail.component.html',
+    styleUrls: ['./media-detail.component.scss']
+    })
 export class MediaDetailComponent implements OnInit {
+    id!: number;
+    mediaVessel!: MediaVessel;
+    lengthFlavourText!: String;
 
-  constructor() { }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private mediaService: MediaService
+    ) { }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.route.paramMap.subscribe(params => {
+            this.id = parseInt(params.get("id")!);            
+            this.mediaService.staticGetMediaVessel(this.id).subscribe(vessel => {
+                console.log("aaaaaaaaaaaaa");
+                
+                this.mediaVessel = vessel;
+                switch (vessel.type) {
+                    case "series":
+                        if (vessel.status == "finished") {
+                            this.lengthFlavourText = `${vessel.currentLength} episodes`;
+                        } else {
+                            this.lengthFlavourText = `${vessel.currentLength} episodes out of ${vessel.finalLength} released`;
+                        }
+                        break;
+                    case "film":
+                        this.lengthFlavourText = `${vessel.currentLength} minutes`;
+                        break;
+                    case "literature":
+                        if (vessel.status == "finished") {
+                            this.lengthFlavourText = `${vessel.currentLength} chapters`;
+                        } else {
+                            this.lengthFlavourText = `${vessel.currentLength} chapters out of ${vessel.finalLength} chapters`;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+    }
 }
