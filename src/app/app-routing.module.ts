91e1ff3b03d9a165123df2b2afc8dffd50448bb8
FSGabrsek <f.gabrsek@gmail.com>
2022-11-17 16:42:14 +0100

    added routing to view-media

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateMediaComponent } from './features/personal-list/pages/create-media/create-media.component';
+import { ViewMediaComponent } from './features/personal-list/pages/view-media/view-media.component';
 
 const routes: Routes = [
-    { path: "add-to-list", component: CreateMediaComponent }
+    { path: "view-media", component: ViewMediaComponent },
+    { path: "create-media", component: CreateMediaComponent },
 ];
 
 @NgModule({
