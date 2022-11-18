bd09c73c816722764eb66ccc028afae6a2520b2f
FSGabrsek <f.gabrsek@gmail.com>
2022-11-18 17:14:28 +0100

    added project-about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectAboutComponent } from './features/about/pages/project-about/project-about.component';
 import { MediaDetailComponent } from './features/personal-list/components/media-detail/media-detail.component';
 import { MediaEditComponent } from './features/personal-list/components/media-edit/media-edit.component';
 import { CreateMediaComponent } from './features/personal-list/pages/create-media/create-media.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
         { path: ":id/edit-media", component: MediaEditComponent },
     ] },
     { path: "create-media", component: CreateMediaComponent },
+    { path: "about", component: ProjectAboutComponent }
 ];
 
 @NgModule({
---
+++ src/app/features/about/about.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectAboutComponent } from './pages/project-about/project-about.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProjectAboutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AboutModule { }
