71e107662440eea92638c0191ced7aa5ac6e7eeb
FSGabrsek <f.gabrsek@gmail.com>
2022-11-24 14:41:27 +0100

    fixed models and input

--- src/app/shared/components/media-list-item/media-list-item.component.spec.ts
+++ src/app/shared/components/media-list-item/media-list-item.component.spec.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { User } from 'src/app/core/models/user.model';
+import { MediaSoul } from 'src/app/features/personal-list/models/mediaSoul.model';
 import { MediaVessel } from 'src/app/features/personal-list/models/mediaVessel.model';
 
 import { MediaListItemComponent } from './media-list-item.component';
+
+const user = new User(
+    0,
+    "jd",
+    "johndoe@email.com",
+    new Date(0)
+);
+
+const soul = new MediaSoul(
+    0,
+    0,
+    new Date(),
+    new MediaVessel(
+        0,
+        "series",
+        "title",
+        `synopsis`,
+        10,
+        10,
+        "finished",
+        new Date(1970, 1, 1),
+        new Date(24 * 3600 * 7 * 1000),
+        user
+    ),
+    user
+)
 
 describe('MediaListItemComponent', () => {
     let component: TestHostComponent;
@@ -30,19 +58,9 @@ describe('MediaListItemComponent', () => {
 
     @Component({
         selector: `test-host-component`,
-        template: `<app-media-list-item [mediaVessel]="testInput"></app-media-list-item>`
+        template: `<app-media-list-item [mediaSoul]="testModel"></app-media-list-item>`
     })
     class TestHostComponent {
-        testInput =  new MediaVessel(
-            0,
-            "series",
-            "title",
-            `synopsis`,
-            10,
-            10,
-            "finished",
-            new Date(1970, 1, 1),
-            new Date(24 * 3600 * 7 * 1000)
-        )
+        testModel = soul
     }
 });
